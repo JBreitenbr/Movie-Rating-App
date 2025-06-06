d666e6be72ba000d9916bbab881d13e65f66e5e2
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-06-06 11:07:08 +0000

    small bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Movie-Rating-App/'
+  base:'/Movie-Rating-App/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
