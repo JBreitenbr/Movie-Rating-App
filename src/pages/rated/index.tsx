eea6df9850a44f07b6a5e5985737d8f92cb02aaa
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-06-08 12:04:06 +0000

    bug fix: now I can switch between 'Movies' and 'TV Shows' on the 'rated page'

--- src/pages/rated/index.tsx
+++ src/pages/rated/index.tsx
@@ -10,11 +10,10 @@ enum DisplayType{
 }
 export const Rated = () => {
   const [activeTabs,setActiveTabs]=useState<DisplayType>(DisplayType.Movies)
-  const [displayType,setDisplayType]=useState(DisplayType.Movies)
+ /* const [displayType,setDisplayType]=useState(DisplayType.Movies)*/
 const {data:ratedMovies,isLoading:isLoadingMovies} = useQuery({queryKey:["ratedMovies"],queryFn:fetchRatedMovies})
 
 const {data:ratedTvShows,isLoading:isLoadingTvShows} = useQuery({queryKey:["ratedTvShows"],queryFn:fetchRatedTvShows})
-  //document.write(JSON.stringify(ratedMovies))
   return (
-    <Container style={{marginTop:"80px"}}><Menu pointing secondary><Menu.Item name="Movies" active={activeTabs==DisplayType.Movies} onClick={()=>setActiveTabs(DisplayType.Movies)} /><Menu.Item name="TV Shows" active={activeTabs==DisplayType.TvShows} onClick={()=>setActiveTabs(DisplayType.TvShows)} /></Menu><Segment>{isLoadingMovies||isLoadingTvShows?(<div>Loading...</div>):(<div style={{marginTop:20}}>{displayType==DisplayType.Movies?(<ColumnDisplay data={ratedMovies.results} displayType={DisplayType.Movies}/>):(<ColumnDisplay data={ratedTvShows.results} displayType={DisplayType.TvShows}/>)}</div>)}</Segment></Container>)
+    <Container style={{marginTop:"80px"}}><Menu pointing secondary><Menu.Item name="Movies" active={activeTabs==DisplayType.Movies} onClick={()=>setActiveTabs(DisplayType.Movies)} /><Menu.Item name="TV Shows" active={activeTabs==DisplayType.TvShows} onClick={()=>setActiveTabs(DisplayType.TvShows)} /></Menu><Segment>{isLoadingMovies||isLoadingTvShows?(<div>Loading...</div>):(<div style={{marginTop:20}}>{activeTabs==DisplayType.Movies?(<div><Header as={"h2"}>Rated Movies</Header><ColumnDisplay data={ratedMovies.results} displayType={DisplayType.Movies}/></div>):(<div><Header as={"h2"}>Rated Tv Shows</Header><ColumnDisplay data={ratedTvShows.results} displayType={DisplayType.TvShows}/></div>)}</div>)}</Segment></Container>)
 }
