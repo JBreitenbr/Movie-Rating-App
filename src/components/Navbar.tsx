b82a2308d15bb433d5b303473f4fd9375296b631
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-06-11 09:10:24 +0000

    added logout functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
-import { Menu } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import { Menu, Button} from 'semantic-ui-react'
+import { Link, useNavigate } from 'react-router-dom'
 export const Navbar = () => {
+  const isLoggedIn=localStorage.getItem("guest_session_id")!==null;
+  const navigate=useNavigate();
+  const handleLogout=()=>{
+    localStorage.removeItem("guest_session_id");
+    navigate("/Movie-Rating-App/auth");
+  }
   return (
     <Menu fixed="top" size="huge">
       
 <Menu.Item as={Link} to="/Movie-Rating-App" style={{fontSize:"1.5rem"}}>Home</Menu.Item>
       <Menu.Item as={Link} to="/Movie-Rating-App/rated" style={{fontSize:"1.5rem"}}>Rated</Menu.Item>
       <Menu.Menu position="right">
-<Menu.Item as={Link} to="/Movie-Rating-App/auth" style={{fontSize:"1.5rem"}}>Auth</Menu.Item></Menu.Menu>
+        {isLoggedIn?(<Menu.Item as={Button} style={{fontSize:"1.5rem"}} onClick={handleLogout}>Logout</Menu.Item> ):(
+<Menu.Item as={Link} to="/Movie-Rating-App/auth" style={{fontSize:"1.5rem"}}>Auth</Menu.Item>)}</Menu.Menu>
     </Menu>
   )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,7 @@ import {Button} from 'semantic-ui-react'
 import {useState} from 'react'
 import {ColumnDisplay} from './column-display'
 import {useQuery} from '@tanstack/react-query'
+import {Navigate} from "react-router-dom";
 import {fetchMovies,fetchTvShows} from './query'
 export enum DisplayType{
     Movies="movies",
@@ -12,6 +13,9 @@ export const Home = () => {
   const [displayType,setDisplayType]=useState(DisplayType.Movies)
 const {data: movieData,isLoading: isLoadingMovies} = useQuery({queryKey:["movies"],queryFn:fetchMovies})
 const {data: tvShowData,isLoading: isLoadingTvShows} = useQuery({queryKey:["tvshows"],queryFn:fetchTvShows})
+  if(!localStorage.getItem("guest_session_id")){
+    return <Navigate to="/Movie-Rating-App/auth"/>
+  }
   return (<div style={{marginTop:80,height:"auto",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}><Button.Group><Button color={displayType==DisplayType.Movies?"blue":undefined} onClick={()=>setDisplayType(DisplayType.Movies)}>Movies</Button><Button color={displayType==DisplayType.TvShows?"blue":undefined} onClick ={()=>setDisplayType(DisplayType.TvShows)}>TV Shows</Button></Button.Group><div>{localStorage.getItem("guest_session_id")}</div>
   {isLoadingMovies||isLoadingTvShows?(<div>Loading...</div>):(<div style={{marginTop:20}}>{displayType==DisplayType.Movies?(<ColumnDisplay data={movieData.results} displayType={DisplayType.Movies}/>):(<ColumnDisplay data={tvShowData.results} displayType={DisplayType.TvShows}/>)}</div>)}
   </div>)
