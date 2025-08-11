8c80dba032cc422369d0caa57ca8906834849c0b
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-08-11 11:24:44 +0000

    cosmetics

--- src/pages/search/column-display.tsx
+++ src/pages/search/column-display.tsx
@@ -1,4 +1,4 @@
-import {Grid,Card,Form} from 'semantic-ui-react'
+import {Grid,Card,Form,Label} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 import {DisplayType} from './index'
 import {useMutation} from '@tanstack/react-query'
@@ -14,15 +14,17 @@ interface DisplayData {
   name?:string;
   vote_average: number;
   release_date: string;
+  rating?: number;
 }
 
 interface Props {
   data: DisplayData[];
   displayType: DisplayType;
+  isRated?: boolean;
 }
 
 export const ColumnDisplay = (props:Props) => {
-const {data,displayType} = props
+const {data,displayType,isRated} = props
 const [rating,setRating] = useState<number>(0)
 const onSuccess=()=>{
   toast.success("Rated Successfully")
@@ -34,5 +36,5 @@ const {mutate: rateMovieMutation} = useMutation({mutationKey:["rateMovie"],mutat
 const rate=displayType==DisplayType.Movies?rateMovieMutation:rateTvShowMutation
     return (<Grid columns={3} stackable centered verticalAlign="top" padded="vertically">{data.map((displayData:DisplayData)=><Grid.Column key={displayData.id}><Card.Group>
 <Link to={`/Movie-Rating-App/${displayType==DisplayType.Movies?"movies":"tvshows"}/${displayData.id}`}>
-<Card fluid image={`https://image.tmdb.org/t/p/original/${displayData.poster_path}`} header={displayType==DisplayType.Movies?displayData.title:displayData.name} meta={displayType==DisplayType.Movies?`Release Date: ${displayData.release_date} | Rating: ${displayData.vote_average}`:`Rating: ${displayData.vote_average}`} description={displayData.overview.slice(0,350)+"..."}/></Link><Form><Form.Group><Form.Field><Form.Input type="number" min="0" max="10" step="0.5" onChange={(e)=>setRating(Number(e.target.value))}  action={{color:"teal",labelPosition:"right",icon:"star",content:"Rate",onClick:()=>rate(displayData.id)}}/></Form.Field></Form.Group></Form></Card.Group></Grid.Column>)}</Grid>);
+<Card style={{height: 720}} fluid image={`https://image.tmdb.org/t/p/original/${displayData.poster_path}`} header={displayType==DisplayType.Movies?displayData.title:displayData.name} meta={displayType==DisplayType.Movies?`Release Date: ${displayData.release_date} | Rating: ${displayData.vote_average}`:`Rating: ${displayData.vote_average}`} description={displayData.overview.slice(0,350)+"..."}/>{isRated && <Label>Your rating: {displayData.rating}</Label>}</Link><Form><Form.Group><Form.Field><Form.Input type="number" min="0" max="10" step="0.5" onChange={(e)=>setRating(Number(e.target.value))}  action={{color:"teal",labelPosition:"right",icon:"star",content:"Rate",onClick:()=>rate(displayData.id)}}/></Form.Field></Form.Group></Form></Card.Group></Grid.Column>)}</Grid>);
   };
